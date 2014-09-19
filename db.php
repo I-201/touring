922ea49a9485c6caa679acaf93cdbdbaf67fd941
Yuki Nirei <rainy554@gamil.com>
2014-09-19 21:42:13 +0900

    データーベースの中身確認OK!

--- db.php
+++ db.php
@@ -1,32 +1,47 @@
+<html>
+<head>
+<title>PHP TEST</title>
+</head>
+<body>
+
 <?php
-$dsn = 'mysql:dbname=bike_sns_php;host=localhost';
-$user = 'root';
-$password = 'root';
+$link = mysql_connect('localhost', 'dbuser', 'dbuser');
 
-try{
-	print("接続完了\n");
-    $dbh = new PDO($dsn, $user, $password);
-
-$dbh->query('SET NAMES sjis');
-
-    $sql = 'select name from image';
-    foreach ($dbh->query($sql) as $row) {
-        print($row['name'].'<br>');
-	}
-
-$dbh->query('SET NAMES sjis');
-	$sql = 'select name ,X(latlng),Y(latlng),ASTEXT(latlng) from image';
-	foreach ($dbh->query($sql) as $row) {
-	        print($row['geom'].'<br>');
-		}
-
-
-}catch (PDOException $e){
-    print('Error:'.$e->getMessage());
-    die();
+if (!$link) {
+    die('接続失敗です。'.mysql_error());
 }
 
-$dbh = null;
+print("<p>接続に成功。</p>");
+
+$db_selected = mysql_select_db('bike_sns_php', $link);
+if (!$db_selected){
+    die('データベース選択失敗です。'.mysql_error());
+}
+
+print("<p>データベース選択成功。</p>");
+
+mysql_set_charset('utf8');
+
+$result = mysql_query('SELECT * FROM users');
+if (!$result) {
+    die('クエリーが失敗しました。'.mysql_error());
+}
+
+
+while ($row = mysql_fetch_assoc($result)) {
+	printf("<p>");
+    print("id=".$row['id']);
+    print(" name=".$row['name']);
+    print(" email=".$row['email']);
+    print(" pass=".$row['password']);
+    print(" create=".$row['created']);
+	printf("</p>");
+}
+
+
+
+mysql_close($link);
 
 ?>
-
+</body>
+</html>
